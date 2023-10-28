d2b55f0edc05b2f62ca1dac01d488879e6d720d9
FriptuT <friptut@gmail.com>
2023-10-28 20:27:15 +0300

    redux workflow

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,29 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import store from './store';
 
-import confetti from 'canvas-confetti';
+const unsubscribe =  store.subscribe(() => {
+  console.log("Store changed!", store.getState());
+  
+})
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+
+store.dispatch({
+  type: "bugAdded",
+  payload: {
+    description: "Bug1"
+  }
+})
+
+unsubscribe();
+
+store.dispatch({
+  type: "bugRemoved",
+  payload: {
+    id: 1
+  }
+})
+
+
+
+
+console.log(store.getState());
